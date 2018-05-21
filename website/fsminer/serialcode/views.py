54a47db3e79f521f583dcecdbec351e07595f8ca
fsminer <pm@yanggan.me>
2018-05-21 22:26:55 +0800

    修改验证流程，兼容误删文件

--- website/fsminer/serialcode/views.py
+++ website/fsminer/serialcode/views.py
@@ -2,7 +2,7 @@ from django.views.decorators.http import require_http_methods
 from django.http import JsonResponse
 from .models import SerialCode
 
-import hashlib, json
+import hashlib, json, traceback
 
 def enctypt(salt, raw_code):
 	raw = hashlib.sha256(raw_code.encode('utf-8'))
@@ -12,29 +12,33 @@ def enctypt(salt, raw_code):
 
 @require_http_methods(["POST"])
 def activate_code(request):
-	received_json_data=json.loads(request.body.decode('utf-8'))
-	mac = received_json_data.get('mac')
-	serialcode = received_json_data.get('serialcode')
+	try:
+		received_json_data=json.loads(request.body.decode('utf-8'))
+		mac = received_json_data.get('mac')
+		serialcode = received_json_data.get('serialcode')
 
-	# validate the request data
-	if not (mac and serialcode):
-		return JsonResponse({"FAILD":"Invalid data"}, status=400)
+		# validate the request data
+		if not (mac and serialcode):
+			return JsonResponse({"FAILD":"Invalid data"}, status=400)
 
-	# If the code exist & not activated
-	serialcode = SerialCode.objects.get(serialcode=serialcode)
-	if serialcode:
+		# If the code exist & not activated
+		try:
+			serialcode = SerialCode.objects.get(serialcode=serialcode)
+		except SerialCode.DoesNotExist:
+			return JsonResponse({"FAILD":"Invalid code"}, status=400)
+
 		auth_key = serialcode.auth_key
 		if auth_key:
-			return JsonResponse({"FAILD":"already activated"}, status=400)
-	else:
-		return JsonResponse({"FAILD":"Invalid code"}, status=400)
+			return JsonResponse({"SUCCESS":serialcode.auth_key}, status=200)
 
-	# activate & save key
-	salt = serialcode.serialcode[2:9]
-	auth_key = enctypt(salt=salt, raw_code=mac)
-	serialcode.mac = mac
-	serialcode.auth_key = auth_key
-	serialcode.save()
-
+		# activate & save key
+		salt = serialcode.serialcode[2:9]
+		auth_key = enctypt(salt=salt, raw_code=mac)
+		serialcode.mac = mac
+		serialcode.auth_key = auth_key
+		serialcode.save()
+	except Exception as e:
+		print('GG----------:', traceback.format_exc())
+		return JsonResponse({"FAILD":"server error"}, status=400)
 	return JsonResponse({"SUCCESS":serialcode.auth_key}, status=200)
 
